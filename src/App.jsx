e2d0d56d6bd642da90d23a11e28f763505d0490b
CasperTheBeagle <146039265+CasperTheBeagle@users.noreply.github.com>
2025-12-30 11:09:00 +0700

    feat/setup: initialize project with vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import './App.css' // หรือถ้าไม่ได้ใช้ไฟล์นี้จะลบออกก็ได้ครับ
+
+function App() {
+  return (
+    <section>
+      <h1>My First React App on Vite</h1>
+    </section>
+  )
+}
+
+export default App
